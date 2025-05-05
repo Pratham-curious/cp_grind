a7055b48ffdd9bd41d6df7ad4308a7b302b4d6a2
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-05-05 18:28:12 +0530

    Time: 17 ms (5.32%) | Memory: 9.2 MB (26.67%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    #define ll long long
+    const long long mod = 1e9+7;
+    ll cal(int n,vector<ll>& dp){
+        if(n==0) return 1;
+        else if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        ll sum = 0;
+        if(n>=2) sum = (sum + cal(n-2,dp))%mod;
+        if(n>=1) sum = (sum + cal(n-1,dp))%mod;
+        if(n>=3) {
+            for(int i=3;n-i>=0;i++) sum = (sum + 2*cal(n-i,dp))%mod;
+        }
+        return dp[n]=sum;
+
+    }
+    int numTilings(int n) {
+        ll sum = 0;
+        vector<ll> dp(n+1,-1);
+        return cal(n,dp);
+        
+    }
+};
