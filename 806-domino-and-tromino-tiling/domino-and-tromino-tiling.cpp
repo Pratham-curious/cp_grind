56b59a572eea1542dcb49f401a0eecde00607802
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-05-05 18:42:35 +0530

    Time: 15 ms (5.32%) | Memory: 9.1 MB (35.55%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     #define ll long long
-    const long long mod = 1e9+7;
-    ll cal(int n,vector<ll>& dp){
-        if(n==0) return 1;
-        if(dp[n]!=-1) return dp[n];
-        ll sum = 0;
-        if(n>=2) sum = (sum + cal(n-2,dp))%mod;
-        if(n>=1) sum = (sum + cal(n-1,dp))%mod;
-        if(n>=3) {
-            for(int i=3;n-i>=0;i++) sum = (sum + 2*cal(n-i,dp))%mod;
-        }
-        return dp[n]=sum;
-
-    }
+    const ll mod = 1e9+7;
     int numTilings(int n) {
-        vector<ll> dp(n+1,-1);
-        return cal(n,dp);
-        
+        vector<ll> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i>=1) dp[i] = (dp[i]+dp[i-1])%mod;
+            if(i>=2) dp[i] = (dp[i]+dp[i-2])%mod;
+            if(i>=3){
+                for(int j=3;i-j>=0;j++) dp[i] = (dp[i] + 2*dp[i-j])%mod;
+            }
+        }
+        return dp[n];
     }
 };
