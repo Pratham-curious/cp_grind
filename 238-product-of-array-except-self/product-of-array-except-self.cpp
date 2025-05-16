02168eaaeaabd46d063ca2509aa3ee2f3815006d
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-05-17 00:45:38 +0530

    Time: 6 ms (8.01%) | Memory: 40.2 MB (79.50%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,20 +5,13 @@ public:
         int n = nums.size();
         vector<int> ans(n,0);
         for(auto it : nums) cnt+=it==0;
-        if(cnt>=2){
-            return ans;
-        }
-        else if(cnt == 1){
-            long long res = 1;
-            for(auto it : nums) if(it != 0 ) res*=it;
+        long long res = 1;
+        for(auto it : nums) if(it != 0 ) res*=it;
+        if(cnt == 1){
             for(int i=0;i<n;i++) if(nums[i]==0) ans[i]=res;
-
         }
-        else{
-            long long res = 1;
-            for(auto it : nums) if(it != 0 ) res*=it;
+        else if(cnt == 0){
             for(int i=0;i<n;i++) ans[i]=res/nums[i];
-
         }
         return ans;
     }
