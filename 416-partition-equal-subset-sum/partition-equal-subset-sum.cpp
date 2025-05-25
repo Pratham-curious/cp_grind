d408f359a9e75b90ca07eb820ecd8b87e0354a22
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-05-25 12:25:18 +0530

    Time: 31 ms (91.48%) | Memory: 14.8 MB (63.82%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    #define ll long long
     bool canPartition(vector<int>& nums) {
-        ll sum = accumulate(nums.begin(),nums.end(),0ll);
+        int n = nums.size();
+        int sum = 0;
+        for(auto it : nums) sum+=it;
         if(sum & 1) return false;
-        ll tar = sum/2;
-        vector<bool> temp(tar+1,false);
-        for(auto it : nums){
-            for(int i=temp.size()-1;i>=0;i--){
-                if(temp[i] && i+it<temp.size()) temp[i+it]=true;
+        sum /=2;
+        vector<int> dp(sum+1,false);
+        dp[0]=true;
+        for(int i=0;i<n;i++){
+            for(int j=sum;j>=nums[i];j--){
+                dp[j] |= dp[j-nums[i]];
             }
-            if(it<temp.size())temp[it]=true;
         }
-        return temp[tar];
+        return dp[sum];
     }
 };
