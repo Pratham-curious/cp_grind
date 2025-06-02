59e3ee35768860666121d7d667cb8ce25dbc3070
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-02 09:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (18.71%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -3,7 +3,7 @@ public:
     #define ll long long
     ll cal(ll n){
         if( n < 0) return 0;
-        return ((n+1)*(n+2))/2;
+        return (n+1)*(n+2)/2;
     }
     long long distributeCandies(int n, int limit) {
         return (cal(n) - 3ll*cal(n-(limit+1)) + 3ll*cal(n - 2*(limit+1)) - cal(n - 3*(limit+1)));
