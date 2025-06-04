c814498fa5dc80350e52575dedc28171eff8a4b2
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-04 19:11:15 +0530

    Time: 221 ms (87.79%) | Memory: 8.8 MB (63.20%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool check(vector<vector<char>>& board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch)
+                return false;
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == ch)
+                return false;
+        }
+        int i = (row / 3) * 3, j = (col / 3) * 3;
+        for (int x = i; x < i + 3; x++) {
+            for (int y = j; y < j + 3; y++) {
+                if (board[x][y] == ch)
+                    return false;
+            }
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board, int row, int col) {
+        if (col >= 9) {
+            col = 0, row = row + 1;
+        }
+        if (row == 9)
+            return true;
+        if (board[row][col] == '.') {
+            for (int j = 1; j <= 9; j++) {
+                char ch = '0' + j;
+                if (check(board, row, col, ch)) {
+                    board[row][col] = ch;
+                    if (solve(board,row,col+1)) return true;
+                }
+                
+            }
+            board[row][col] = '.';
+            return false;
+        }
+
+        return solve(board,row,col+1);
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        bool check = solve(board, 0, 0);
+    }
+};
