79a062f36c0fadf0f68978cdb037186285c67485
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-04 09:44:45 +0530

    Time: 75 ms (41.05%) | Memory: 169.4 MB (14.93%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -3,12 +3,12 @@ public:
     string answerString(string word, int numFriends) {
         set<string> st;
         int n = word.size();
-        string ans;
+        string ans ="";
         if(numFriends == 1) return word;
         for(int i=0;i<n;i++){
-            ans = word.substr(i,min(n-numFriends+1,n-i));
-            st.insert(ans);
+            ans = max(ans,word.substr(i,min(n-numFriends+1,n-i)));
+            
         }
-        return *st.rbegin();
+        return ans;
     }
 };
