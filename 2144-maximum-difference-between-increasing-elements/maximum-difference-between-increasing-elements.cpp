7d04ff38d80f35fa8603f79165fd68bb914e8270
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-16 12:02:18 +0530

    Time: 1 ms (28.41%) | Memory: 12.8 MB (5.67%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -4,10 +4,9 @@ public:
         set<int> st;
         int maxi = -1;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if(!st.empty()){
-                maxi = max(maxi,nums[i]-*st.begin());
-            }
+        st.insert(nums[0]);
+        for (int i = 1; i < n; i++) {
+            maxi = max(maxi,nums[i]-*st.begin());
             st.insert(nums[i]);
         }
         return maxi==0?-1:maxi;
