201b1789f0119398f36783e1066594438907f909
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-26 14:10:41 +0530

    Time: 2 ms (35.45%) | Memory: 10.5 MB (21.69%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-
+        
         int n = s.size();
-        vector<int> pref(n, 0);
-        if (s[0] == '0')
-            pref[0] = 1;
-        for (int i = 1; i < n; i++)
-            pref[i] = pref[i - 1] + (s[i] == '0');
-
-        long long ans = pref[n - 1];
+        vector<int> pref(n,0);
+        if(s[0]=='0') pref[0]=1;
+        for(int i=1;i<n;i++) pref[i] = pref[i-1]+(s[i]=='0');
+        long long ans = pref[n-1];
         long long curr = 0;
-        for (long long i = n - 1, j = 0; i >= 0 && j < 60; i--, j++) {
-            if (s[i] == '1')
-                curr += (1LL << j);
-            if (curr > k)
-                break;
-            ans = max(ans, (i > 0 ? pref[i - 1] : 0) + n-i);
+        for(long long i=n-1,j = 0;i>=0 && j < 60 ;i--,j++){
+            long long val = n-i;
+            curr += (s[i]=='1')*(1LL << j);
+            if(curr <= k) ans = max(ans,(i>0 ? pref[i-1] : 0) +val);
         }
-
         return ans;
     }
 };
