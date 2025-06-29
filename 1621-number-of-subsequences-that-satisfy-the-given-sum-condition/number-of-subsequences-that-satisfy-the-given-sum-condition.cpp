a7c9806acfd398c7d6fb2a9959eac119060de5f1
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-06-29 09:41:29 +0530

    Time: 62 ms (5.98%) | Memory: 51.7 MB (78.26%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -2,39 +2,28 @@ class Solution {
 public:
     #define ll long long
     const ll mod = 1e9+7;
-    ll expo(ll a,ll b){
+
+    ll expo(ll a, ll b){
         ll res = 1;
-        while(b> 0){
-            if(b & 1) res =(res*a)%mod;
-            a = (a*a) % mod;
+        while(b>0){
+            if(b&1) res =(res*a)%mod;
+            a = (a*a)%mod;
             b>>=1;
         }
         return res;
     }
-    int search(vector<int>& nums,int low, int tar){
-        int high = nums.size()-1;
-        int ans = -1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]<=tar) {
-                low = mid+1;
-                ans = mid;
-            } else high = mid-1;
-        }
-        return ans;
-    }
-
 
     int numSubseq(vector<int>& nums, int target) {
+        ll ans = 0;
+        ll n = nums.size();
         sort(nums.begin(),nums.end());
-        int ans = 0;
-        int n = nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]<=target){
-                int idx = search(nums,i,target-nums[i]);
-                if(idx == -1 ) break;
-                ans = (ans+ expo(2,idx-i))%mod;
-            }
+            int val = target-nums[i];
+            auto it = upper_bound(nums.begin(),nums.end(),val)-nums.begin();
+            if(it == 0) continue;
+            --it;
+            ll len = (it-i >= 0) ? expo(2,it-i) : 0;
+            ans = (ans+(len)%mod)%mod;
         }
         return ans;
     }
