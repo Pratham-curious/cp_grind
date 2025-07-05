3a4123b092c14a8591a7d589dc0af704ff926d4f
Pratham-curious <114573879+Pratham-curious@users.noreply.github.com>
2025-07-06 01:14:33 +0530

    Time: 55 ms (89.47%) | Memory: 177.5 MB (89.47%) - LeetSync

--- 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
+++ 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
 #define ll long long 
     long long minCost(int m, int n, vector<vector<int>>& waitCost) {
-        vector<vector<ll>> dp(m,vector<ll>(n,LLONG_MAX));
-        dp[m-1][n-1] = m*n;
+        vector<ll> dp(n,0);
         for(int i=m-1;i>=0;i--){
+            vector<ll> temp(n,LLONG_MAX);
             for(int j=n-1;j>=0;j--){
-                if( i == m-1 && j == n-1) continue;
+                if( i == m-1 && j == n-1) {temp[j]=m*n; continue;}
                 ll mini = LLONG_MAX;
-                if(i+1<m && j <n) mini = min(mini,dp[i+1][j]);
-                if(i<m && j+1 <n) mini = min(mini,dp[i][j+1]);
-                dp[i][j] = (i+1)*(j+1) + waitCost[i][j] + mini;
+                if(i+1<m && j<n) mini = min(mini,dp[j]);
+                if(i<m && j+1 <n) mini = min(mini,temp[j+1]);
+                temp[j] = (i+1)*(j+1) + waitCost[i][j] + mini;
             }
+            dp = temp;
         }
-        return dp[0][0]-waitCost[0][0];
+        return dp[0]-waitCost[0][0];
     }
 };
