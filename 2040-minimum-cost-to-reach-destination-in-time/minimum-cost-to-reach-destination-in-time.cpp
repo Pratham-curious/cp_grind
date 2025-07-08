aeebbc48829cba5ab4101f99520bace46e065523
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2025-07-08 19:53:46 +0530

    Time: 36 ms (91.67%) | Memory: 30.4 MB (89.58%) - LeetSync

---
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minCost(int maxTime, vector<vector<int>>& edges,
+                vector<int>& passingFees) {
+        int n = passingFees.size();
+        int source = 0, destination = n - 1;
+        vector<vector<pair<int, int>>> adjacencyList(n);
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0], v = edges[i][1], time = edges[i][2];
+            adjacencyList[u].push_back({v, time});
+            adjacencyList[v].push_back({u, time});
+        }
+        // to save the cost i will make a distance array;
+        vector<int> cost(n, INT_MAX);
+        vector<int> Time(n,maxTime+1);
+        cost[source] = passingFees[source];
+
+        // queue -> {timeTaken till that node, node, cost Till now }
+        using T = tuple<int,int,int>;
+        priority_queue<T,vector<T>,greater<T>> qu;
+        qu.push({cost[source],source,0});
+
+        while(!qu.empty()){
+            auto [c,b,a] = qu.top(); // a-> time taken till that node, that
+                                         // particular node, cost till now
+            qu.pop();
+            if(b == destination) return c;
+
+            if (a > maxTime)
+                continue;
+            
+
+            for (auto [node, time] : adjacencyList[b]) {
+                if (a + time <= maxTime && (c + passingFees[node] < cost[node] || a+time < Time[node])) {
+                    
+                        Time[node] = a+time;
+                        cost[node] = c+passingFees[node];
+                    
+                    qu.push({c+passingFees[node],node,a+time});
+                
+                }
+            }
+        }
+            
+                return -1;
+                
+        
+    }
+};
