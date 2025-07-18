3b5e11166d028c2f6c9a70fcb7069d739d65db5b
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2025-07-18 11:21:23 +0530

    Time: 907 ms (5.41%) | Memory: 337.5 MB (5.16%) - LeetSync

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    #define ll long long
+    long long minimumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int m = n/3;
+        vector<ll> mini(n,0), maxi(n,0);
+        multiset<int> st;
+        ll sum = 0;
+        for(int i=n-1;i>=0;i--){
+            st.insert(nums[i]);
+            sum+=nums[i];
+            if(st.size()>m){
+                sum-=*st.begin();
+                st.erase(st.begin());
+            }
+            if(st.size()!= m) maxi[i] = LLONG_MIN;
+            else maxi[i] = sum;
+        }
+        while(!st.empty()){
+            st.erase(st.begin());
+        }
+        sum = 0;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+            sum+=nums[i];
+            if(st.size()>m){
+                sum-=*st.rbegin();
+                st.erase(st.find(*st.rbegin()));
+            }
+            if(st.size()!= m) mini[i] = LLONG_MAX;
+            else mini[i] = sum;
+        }
+        ll ans = LLONG_MAX;
+        for(int i=0;i<n-1;i++){
+            if(mini[i] == LLONG_MAX || maxi[i+1] == LLONG_MIN) continue;
+            ans = min(ans,mini[i]-maxi[i+1]);
+        }
+        return ans;
+    }
+};
