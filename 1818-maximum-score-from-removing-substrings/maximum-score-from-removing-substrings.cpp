abca81af6f922a35a42e7d595f26c56b5c489052
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2025-07-23 10:42:53 +0530

    Time: 41 ms (65.95%) | Memory: 24.3 MB (82.01%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -5,71 +5,70 @@ public:
         // x -> ab, y -> ba
         int a1 = 0, a2 = 0;
         stack<char> st;
-        for (int i = n - 1; i >= 0; i--) {
+        if (y >= x) {
+            for (int i = n - 1; i >= 0; i--) {
 
-            if (s[i] == 'b') {
-                if (st.empty() || st.top() != 'a') {
-                    st.push(s[i]);
-                } else {
-                    st.pop();
-                    a1 += y;
-                }
-            } else
-                st.push(s[i]);
-        }
-         if (!st.empty()) {
-            stack<char> temp;
-            while (!st.empty()) {
-                char curr = st.top();
-                st.pop();
-                if(temp.empty()){
-                    temp.push(curr);
-                }
-                else{
-                    if(curr == 'b'){
-                        if(temp.top() == 'a'){
-                            temp.pop();
-                            a1+=x;
-                        }
-                        else temp.push(curr);
+                if (s[i] == 'b') {
+                    if (st.empty() || st.top() != 'a') {
+                        st.push(s[i]);
+                    } else {
+                        st.pop();
+                        a1 += y;
                     }
-                    else{
+                } else
+                    st.push(s[i]);
+            }
+            if (!st.empty()) {
+                stack<char> temp;
+                while (!st.empty()) {
+                    char curr = st.top();
+                    st.pop();
+                    if (temp.empty()) {
                         temp.push(curr);
+                    } else {
+                        if (curr == 'b') {
+                            if (temp.top() == 'a') {
+                                temp.pop();
+                                a1 += x;
+                            } else
+                                temp.push(curr);
+                        } else {
+                            temp.push(curr);
+                        }
                     }
                 }
             }
-        }
+        } else {
 
-        for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
 
-            if (s[i] == 'b') {
-                if (st.empty() || st.top() != 'a') {
-                    st.push(s[i]);
-                } else {
-                    st.pop();
-                    a2 += x;
-                }
-            } else
-                st.push(s[i]);
-        }
-        if (!st.empty()) {
-            stack<char> temp;
-            while (!st.empty()) {
-                char curr = st.top();
-                st.pop();
-                if(temp.empty()){
-                    temp.push(curr);
-                }
-                else{
-                    if(curr == 'b'){
-                        if(temp.top() == 'a'){
-                            temp.pop();
-                            a2+=y;
-                        }
-                        else temp.push(curr);
+                if (s[i] == 'b') {
+                    if (st.empty() || st.top() != 'a') {
+                        st.push(s[i]);
+                    } else {
+                        st.pop();
+                        a2 += x;
                     }
-                    else{
+                } else
+                    st.push(s[i]);
+            }
+            if (!st.empty()) {
+                stack<char> temp;
+                while (!st.empty()) {
+                    char curr = st.top();
+                    st.pop();
+                    if (temp.empty()) {
                         temp.push(curr);
+                    } else {
+                        if (curr == 'b') {
+                            if (temp.top() == 'a') {
+                                temp.pop();
+                                a2 += y;
+                            } else
+                                temp.push(curr);
+                        } else {
+                            temp.push(curr);
+                        }
                     }
                 }
             }
