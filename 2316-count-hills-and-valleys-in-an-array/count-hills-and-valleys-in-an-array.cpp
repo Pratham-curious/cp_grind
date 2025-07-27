06a74f3a410648449bebab828bdb1544ee1e9bca
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2025-07-27 12:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (77.89%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -3,32 +3,46 @@ public:
     int countHillValley(vector<int>& nums) {
         int cnt = 0;
         int n = nums.size();
-        int hill = -1, last = -1;
-        
-        for(int i=1;i<n-1;i++){
-            if(last == nums[i]){
-                if(nums[i] == nums[i+1]) continue;
-                if(hill == 0 && last < nums[i+1]) {cnt++;}
-                if(hill == 1 && last > nums[i+1]) {cnt++;}
-                
+        int hill = -1;
 
-            }
-            else if(nums[i] == nums[i+1]){
-                last = nums[i];
-                if(last > nums[i-1]) hill = 1;
-                else if(last < nums[i-1]) hill = 0;
-                else last = -1;
-            }
-            else{
-                if(nums[i-1] > nums[i] && nums[i] < nums[i+1]){
-                    last = -1;
-                    cnt++;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == nums[i - 1] && nums[i] == nums[i + 1])
+                continue;
+            else if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                cnt++;
+                hill = -1;
+            } else if (nums[i] < nums[i - 1] && nums[i] < nums[i + 1]) {
+                hill = -1;
+                cnt++;
+            } else if (nums[i] == nums[i - 1] && nums[i] > nums[i + 1]) {
+                if (hill == -1) {
+                    hill = 0;
+                } else {
+                    if (hill == 1) {
+                        cnt++;
+                        hill = -1;
+                    }
                 }
-                else if(nums[i-1] < nums[i] && nums[i] > nums[i+1]){
-                    last = -1;
-                    cnt++;
+            } else if (nums[i] == nums[i - 1] && nums[i] < nums[i + 1]) {
+                if (hill == -1) {
+                    hill = 1;
+                } else {
+                    if (hill == 0){
+                        cnt++;
+                        hill = -1;
+                    }
                 }
+            } else if (nums[i] > nums[i - 1] && nums[i] == nums[i + 1]) {
+                if (hill == -1) {
+                    hill = 1;
+                } 
+            } else if (nums[i] < nums[i - 1] && nums[i] == nums[i + 1]) {
+                if (hill == -1) {
+                    hill = 0;
+                } 
             }
+
+
         }
         return cnt;
     }
