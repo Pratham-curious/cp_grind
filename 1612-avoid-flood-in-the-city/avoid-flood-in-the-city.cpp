7a450806246290872594647874f1869d7e8e16a7
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2025-10-07 08:54:38 +0530

    Time: 198 ms (19.49%) | Memory: 117 MB (22.88%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int> ans(n, -1);
+        set<int> zro;
+        map<int,int> mp;
+
+        for (int i = 0; i < n; i++) {
+            if (rains[i] > 0) {
+                if (!mp.empty() && mp.find(rains[i]) != mp.end()) {
+                    if (zro.empty())
+                        return vector<int>{};
+                    auto it = zro.lower_bound(mp[rains[i]]);
+                    if(it == zro.end()){
+                         return vector<int>{};
+                    }
+                    int idx = *it;
+                    zro.erase(*it);
+                    ans[idx] = rains[i];
+                    mp[rains[i]] = i;
+                } else {
+                    mp[rains[i]] = i;
+                }
+            } else {
+                zro.insert(i);
+            }
+        }
+        while (!zro.empty()) {
+            int idx = *zro.begin();
+            zro.erase(zro.begin());
+            ans[idx] = 1;
+        }
+        return ans;
+    }
+};
