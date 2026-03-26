4515b91c8a0f5a195cee4581c5980e630a634947
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-03-26 11:43:18 +0530

    Time: 2240 ms (16.92%) | Memory: 409 MB (46.15%) - LeetSync

---
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+#define ll long long
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        vector<ll> pref(n,0),col(m,0);
+
+        map<ll,ll> m1,m2;
+
+
+
+        for(int i=0;i<n;i++){
+            if(i > 0) pref[i]+=pref[i-1];
+            for(int j=0;j<m;j++){ 
+                pref[i]+=grid[i][j];
+                m1[grid[i][j]]++;
+            }
+        }
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<m;j++) {
+                m1[grid[i][j]]--;
+                m2[grid[i][j]]++;
+            }
+            ll x  = pref[i],y = pref[n-1]-pref[i];
+            if(x == y) return true;
+            else if(x > y){
+                if(i> 0 && m >1 &&  m2[x-y] > 0) return true;
+                if(x-y == grid[0][0] || x-y == grid[0][m-1]) return true;
+            }
+            else {
+                if(n-1-i>1 && m > 1 && m1[y-x] >0){
+                    return true;
+                }
+                if(y-x == grid[n-1][0] || y-x == grid[n-1][m-1]) return true;
+            } 
+            if(m == 1){
+                if(x-y == grid[i][0] || x- y == grid[0][0]) return true;
+                if(y-x == grid[i+1][0] || y-x == grid[n-1][0]) return true;
+            }   
+        }
+        m1.clear();
+        m2.clear();
+
+        for(int i=0;i<m;i++){
+            if(i > 0) col[i]+=col[i-1];
+            for(int j=0;j<n;j++){ 
+                col[i]+=grid[j][i];
+                m1[grid[j][i]]++;
+            }
+        }
+        for(int i=0;i<m-1;i++){
+            for(int j=0;j<n;j++) {
+                m1[grid[j][i]]--;
+                m2[grid[j][i]]++;
+            }
+            ll x  = col[i],y = col[m-1]-col[i];
+            if(x == y) return true;
+            else if(x > y){
+                if(i> 0  && n > 1 &&  m2[x-y] > 0) return true;
+                if(x-y == grid[0][0] || x-y == grid[n-1][0]) return true;
+            }
+            else{
+                if(m-1-i > 1 && n>1 &&   m1[y-x] >0) return true;
+                if(y-x == grid[0][m-1] || y-x == grid[n-1][m-1]) return true;
+            }
+            if(n == 1){
+                if(x-y == grid[0][i] || x- y == grid[0][0]) return true;
+                if(y-x == grid[0][i+1] || y-x == grid[0][m-1]) return true;
+            }
+        }
+
+        return false;
+
+
+    }
+};
