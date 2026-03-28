bda5520c1be8ac2f137238d21d0a8a592577ea4e
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-03-28 12:38:52 +0530

    Time: 54 ms (9.64%) | Memory: 76 MB (55.42%) - LeetSync

--- 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -30,63 +30,63 @@ public:
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
-                if(lcp[i][j] == 0) continue;
-                if(v[j] != -1) uni(v[j], i, parent, sz);
+                if (lcp[i][j] == 0)
+                    continue;
+                if (v[j] != -1)
+                    uni(v[j], i, parent, sz);
                 v[j] = i;
             }
         }
-    
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (lcp[i][j] != lcp[j][i])
-                return "";
-            if (i == j && lcp[i][j] != n - i)
-                return "";
-            if (lcp[i][j] > n - i)
-                return "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (lcp[i][j] != lcp[j][i])
+                    return "";
+                if (i == j && lcp[i][j] != n - i)
+                    return "";
+                if (lcp[i][j] > n - i)
+                    return "";
 
-            if (v[i] != -1 && v[j] != -1) {
-                int pa = findparent(v[i], parent);
-                int pb = findparent(v[j], parent);
-                if (pa == pb) {
-                    if (lcp[i][j] == 0)
-                        return "";
-                    if (i + 1 < n && j + 1 < n) {
-                        if (lcp[i][j] != 1 + lcp[i + 1][j + 1])
+                if (v[i] != -1 && v[j] != -1) {
+                    int pa = findparent(v[i], parent);
+                    int pb = findparent(v[j], parent);
+                    if (pa == pb) {
+                        if (lcp[i][j] == 0)
                             return "";
+                        if (i + 1 < n && j + 1 < n) {
+                            if (lcp[i][j] != 1 + lcp[i + 1][j + 1])
+                                return "";
+                        }
                     }
                 }
             }
         }
-    }
 
-    unordered_map<int, char> mp;
-    int j = 0;
+        unordered_map<int, char> mp;
+        int j = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (v[i] == -1) {
-            if (j >= 26)
-                return "";
-            word[i] = 'a' + j;
-            j++;
-        } else {
-            int pa = findparent(v[i], parent);
-            if (mp.find(pa) != mp.end()) {
-                word[i] = 'a' + mp[pa];
-            } else {
+        for (int i = 0; i < n; i++) {
+            if (v[i] == -1) {
                 if (j >= 26)
                     return "";
-                mp[pa] = j;
-                word[i] = 'a' + mp[pa];
+                word[i] = 'a' + j;
                 j++;
+            } else {
+                int pa = findparent(v[i], parent);
+                if (mp.find(pa) != mp.end()) {
+                    word[i] = 'a' + mp[pa];
+                } else {
+                    if (j >= 26)
+                        return "";
+                    mp[pa] = j;
+                    word[i] = 'a' + mp[pa];
+                    j++;
+                }
             }
         }
+        string ans = "";
+        for (auto it : word)
+            ans += it;
+        return ans;
     }
-    string ans = "";
-    for (auto it : word)
-        ans += it;
-    return ans;
-}
-}
-;
+};
