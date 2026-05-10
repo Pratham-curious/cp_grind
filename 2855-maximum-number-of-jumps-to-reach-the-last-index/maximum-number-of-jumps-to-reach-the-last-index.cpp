b90704e1848cc8897f88f4bfe7e7848a987c6990
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-05-10 10:21:00 +0000

    Time: 21 ms (49.38%) | Memory: 68.4 MB (69.25%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+/*
+
+observation :
+
+1. i can go only forward, so maximum possible jump is any way n-1
+2. if i have 2 , 5,10 and target is 8 so i will jump tp 5 first then 10 ->
+    -> that means if i can jump to j from i , i<k<j , such that nums[i] < nums[k]<nums[j] i can jump.
+3.can se as dp[i], where dp[i] is maximum jump you can take to reach i, since we can go only forward i will se only the previous indexs
+    -> dp[i] = max(1 + dp[k]), k < i and abs(nums[i]-nums[k]) <= target;
+
+*/
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> dp(n,-1);
+        dp[0] = 0;
+
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(abs(nums[j]-nums[i]) <= target && dp[j] != -1) dp[i] = max(dp[i],1+dp[j]);
+            }
+        }
+        return dp[n-1];
+    }
+};
