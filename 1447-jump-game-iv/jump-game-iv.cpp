b772918f4a91e33d0b18b27b1803b5a3e03bd3cc
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-05-18 19:46:19 +0000

    Time: 1028 ms (5.02%) | Memory: 80.6 MB (48.27%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return 0;
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<n;i++) mp[arr[i]].push_back(i);
+
+
+
+        vector<bool> vis(n,false);
+        vis[0] = true;
+        vis[1] = true;
+
+        queue<int> qu;
+        int cnt = 1;
+        if(arr[1] != arr[0]) qu.push(1);
+        for(auto it : mp[arr[0]]) {
+            vis[it] = true;
+            if(it != 0 ) qu.push(it);
+        }
+        
+        set<int> st;
+        st.insert(arr[0]);
+
+        while(!qu.empty()){
+            int sz = qu.size();
+
+            while(sz--){
+                int curr = qu.front();
+                qu.pop();
+                cout<<curr<<endl;
+
+                if(curr == n-1) return cnt;
+
+                if(st.find(arr[curr]) == st.end()){
+                    //cout<<"adding : "<<arr[curr]<<" : ";
+                    for(auto it : mp[arr[curr]]){
+                        
+                        if(!vis[it]){
+                            //cout<<it<<" ";
+                            vis[it] = true;
+                            qu.push(it);
+                        }
+                    }
+                  //  cout<<endl;
+                    st.insert(arr[curr]);
+                }
+
+                int prev = curr-1, nx = curr+1;
+
+                if(prev >= 0 && !vis[prev]){
+                    qu.push(prev);
+                    vis[prev] = true;
+                }
+                if(nx < n && !vis[nx]){
+                    qu.push(nx);
+                    vis[nx] = true;
+                }
+            }
+            cout<<"iteration : "<<cnt<<endl;
+            cnt++;
+        }
+        return cnt;
+    }
+};
