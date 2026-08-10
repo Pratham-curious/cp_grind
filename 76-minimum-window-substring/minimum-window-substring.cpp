d2a35ca2b35646f24abde1dd3e893ed1dd092de4
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-08-10 20:56:31 +0530

    Time: 190 ms (5.07%) | Memory: 12.9 MB (5.03%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,40 +1,65 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        map<char, int> mp;
-        for (auto it : t)
-            mp[it]++;
 
+    pair<int,int> check(int& mid,string& s,map<char,int>& f,int tot){
         int n = s.size();
-        int left = 0;
-        int cnt = 0;
-        int l = 0, len = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            if (mp.find(s[i]) != mp.end()) {
-                mp[s[i]]--;
-                if (mp[s[i]] == 0)
-                    cnt++;
+     //   cout<<"for m : "<<mid<<endl;
+
+        map<char,int> temp;
+        for(auto it : f) temp[it.first] = it.second;
+
+        for(int i=0;i<mid;i++){
+            if(f[s[i]] > 0){
+                if(temp[s[i]] > 0) tot--;
+                temp[s[i]]--;
             }
-            
-                while (cnt == mp.size()) {
-                    if (i - left + 1 < len) {
-                        len = i- left + 1;
-                        l = left;
-                    }
-                    if (mp.find(s[left]) != mp.end()) {
-                        mp[s[left]]++;
-                        if (mp[s[left]] == 1) {
-                            cnt--;
-                            left++;
-                            break;
-                        }
-                    }
-                    left++;
-                }
-            
         }
-        if (len == INT_MAX)
-            return "";
-        return s.substr(l, len);
+    //    cout<<" current (l,r) : "<<0<<", "<<mid-1<<" , with tot : "<<tot<<endl;
+        if(tot == 0) return {0,mid-1};
+
+        for(int i=mid,left = 0; i<n && left < n; i++,left++){
+            if(f[s[i]]> 0) {
+                if(temp[s[i]] > 0) tot--;
+                temp[s[i]]--;
+            }
+            if(f[s[left]] > 0){
+               // cout<<temp[s[left]]<<endl;
+
+                if(temp[s[left]] >=  0) tot++;
+                temp[s[left]]++;
+            }
+          //  cout<<"current (l,r) : "<<left+1<<", "<<i<<" , with tot : "<<tot<<endl;
+            if(tot == 0) return {left+1,i};
+        }
+        return {-1,-1};
+
+    }
+    string minWindow(string s, string t) {
+        map<char,int> f;
+
+        int n = s.size(), m = t.size();
+        for(auto it : t) f[it]++;
+
+        int tot = 0;
+        for(auto it : f ) tot += it.second;
+       
+
+        int l = -1,r =-1;
+
+        int low = 1,high = n;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            auto it = check(mid,s,f,tot);
+            if(it.first != -1) {
+                l = it.first ;
+                r = it.second;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+
+        if(low > n) return "";
+        return s.substr(l,r-l+1);
+        
     }
 };
