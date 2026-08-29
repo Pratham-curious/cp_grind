c9c4fc20251de1bd5732b2a706a5cc5fe757916e
Pratham Agarwal <114573879+Pratham-curious@users.noreply.github.com>
2026-08-29 09:28:46 +0530

    Time: 421 ms (18.59%) | Memory: 199.6 MB (46.20%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+/*
+1. 
+
+
+
+*/
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        priority_queue<int> pq;
+        vector<vector<int>> v(n,vector<int>(2,0));
+        for(int i=0;i<n;i++){
+            v[i] = vector<int>{nums[i],i};
+        }
+        sort(v.begin(),v.end());
+        int j = 0;
+        vector<int> ans(n,0);
+
+        for(int i=0;i<n;i++){
+            if(i == 0 || (v[i][0] - v[i-1][0] <= limit)){
+               pq.push(v[i][1]);  
+            }
+            else{
+                for(int k = i-1;k>=j;k--){
+                    int idx = pq.top();
+                    pq.pop();
+                    ans[idx] = v[k][0];
+                }
+                pq.push(v[i][1]);
+                j = i;
+            } 
+        }
+        for(int i = n-1;i>=j;i--){
+            int idx = pq.top();
+            pq.pop();
+            ans[idx] = v[i][0];
+        }
+
+        return ans;
+    }
+};
